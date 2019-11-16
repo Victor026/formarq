072dcd2b21733d527116e585d49175cdd9d21d66
Victor Belo <victorbelo1997@gmail.com>
2019-11-16 08:06:33 -0300

    Agora conversão Invert gera arquivo .inv em branco

--- formarq.c
+++ formarq.c
@@ -6,22 +6,21 @@ FILE *conversaoInvert(FILE *caminho, char diretorio[]);
 //FILE *conversaoUToD(FILE *caminho);
 //FILE *conversaoDToU(FILE *caminho);
 //FILE *conversaoCript(FILE *caminho);
-int somaUm(char ch);
-char *ext(char novoCaminho[]);
+char *ext(char novoDiretorio[]); //Função que tira a extensão de um arquivo
 
 int main(int argc, char *argv[]) {
 
 	FILE *path;
 	char* comando;
 
-	path = fopen(argv[1], "r+");
-	comando = argv[2];
+	path = fopen(argv[1], "r+"); //Atribui o caminho do arquivo inserido à variavel path
+	comando = argv[2]; //Atribui o segundo argumento ao comando
 
 	if(path == NULL) {
 		printf("Caminho inexistente!\n");
 	} else {
 		if(argc == 3) {
-		switch(*comando) {
+		switch(*comando) { //Verifica o comando inserido caso três argumentos sejam inseridos
 			case 'I':
 				printf("Conversão Invert\n");
 				conversaoInvert(path, argv[1]);
@@ -41,7 +40,7 @@ int main(int argc, char *argv[]) {
 				printf("Argumentos inválidos... [I = Invert; U = UNIX to DOS; D = DOS to UNIX]\n");
 		}
 	} else {
-		if(argc == 4) {
+		if(argc == 4) { //Verifica o comando inserido caso quatro argumentos sejam inseridos
 			switch(*comando) {
 				case 'C':
 					printf("Conversão Cript\n");
@@ -62,23 +61,38 @@ int main(int argc, char *argv[]) {
 FILE *conversaoInvert(FILE *caminho, char diretorio[]) {
 
 	char caractere;
-	char novoDiretorio[strlen(diretorio)+2];
+	char novoDiretorio[strlen(diretorio)+2]; //String do novo arquivo que será criado
 	strcpy(novoDiretorio, diretorio);
-	FILE *novoArquivo;
+	FILE *novoArquivo; //Arquivo em si que será criado
 	char *x;
 	int i;
 
-	printf("Novo diretório: %s\n", novoDiretorio);
+	x = ext(novoDiretorio); //Tira a extensão do arquivo
+	strcpy(novoDiretorio, x);
+	strcat(novoDiretorio, ".inv");
 
-	while(caractere = fgetc(caminho) != EOF) {
-		fputc(somaUm(caractere), novoArquivo);
+	novoArquivo = fopen(novoDiretorio, "w+");
+
+	if(x != NULL) {
+		printf("Novo diretório: %s\n", novoDiretorio);
+	} else {
+		strcat(novoDiretorio, ".inv");
 	}
+
 
 	return novoArquivo;
 }
 
-int somaUm(char ch) {
-	int chSomaUm;
+char *ext(char novoDiretorio[]) {
+int i;
+char *x;
+for(i = strlen(novoDiretorio)-1; i && novoDiretorio[i] != "."; i--) { //Loop desde o último caractere até o primeiro
+	if(novoDiretorio[i] == '.') { //Caso ele ache um ponto
+		novoDiretorio[i] = '\0';  //Corta a string
+	} else {
+		x = novoDiretorio;
+	}
+}
 
-	return chSomaUm;
+return novoDiretorio;
 }
