2a2275eb05297bd1d33c2bb5b5b96873dbc7120a
Victor Belo <victorbelo1997@gmail.com>
2019-11-20 12:37:14 -0300

    Programa finalizado e README ajustado

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
 ** -------------- BEM VINDO AO PROGRAMA FORMARQ -------------- **
 
-O Formarq foi desenvolvido com o propósito de criptografar o arquivo trocando a extensão do mesmo.
+** PARA QUE SERVE **
 
-Também há a opção de converter arquivos .txt do modo de leitura Linux/Mac para Windows e vice-versa.
+O Formarq foi desenvolvido com propósitos múltiplos:
+
+CRIPTOGRAFIA
+Formarq é capaz de criptografar arquivos usando criptografia INVERT ou CRIPT.
+
+CONVERSÃO DE ARQUIVOS TEXTO ENTRE UNIX E DOS
+Também é possível converter arquivos com quebra de linha CR (sistema UNIX) para LF/CR (sistema DOS) e vice-versa.
+
+** COMO FUNCIONA **
+
+CRIPTOGRAFIA INVERT
+Esta criptografia usa o sistema de "complemento de um" para criptografar o arquivo.
+Após a conversão será gerado um arquivo com o mesmo nome do original porém com extensão .inv e com o conteúdo criptografado.
+Quando o arquivo com extensão .inv for criptografado novamente será gerado um arquivo .dnv com o conteúdo do arquivo original.
+
+CRIPTOGRAFIA CRIPT
+Já esta criptografia usa o sistema de "ou exclusivo" entre o arquivo e um chave que será passada pelo usuário.
+Após a conversão será gerado um arquivo com o mesmo nome do original porém com extensão .crp e com o conteúdo criptografado.
+Quando o arquivo com extensão .crp for criptografado novamente será gerado um arquivo .drp com o conteúdo do arquivo original.
+
+CONVERSÃO UNIX TO DOS
+Esta conversão gerará um arquivo .dos com o sistema DOS de quebra de linha (LF/CR).
+Quando o arquivo com extensão .dos é convertido novamente, será gerado um arquivo com extensão .unx que voltará a ter o sistema UNIX de qubra de linha (CR).
+
+** COMO UTILIZAR **
+
+O programa exige alguns argumentos que serão passados no terminal, sendo eles:
+Primeiro argumento: Script do formarq. (./formarq)
+Segundo argumento: Caminho do arquivo a ser modificado. (Ex.: ~/Documentos/teste.txt)
+Terceiro argumento: Comando a ser executado. (C para CRIPT, U para UNIX to DOS, I para INVERT).
+Quarto argumento: Somente usado na conversão CRIPT, será colocado a chave para a conversão.
+
+Exemplos:
+./formarq ~/Documentos/teste.txt I fará a criptografia INVERT no arquivo teste.txt.
+
+./formarq ~/Documentos/teste.txt C MORANGO fará a criptografia CRIPT no arquivo teste.txt usando a chave MORANGO.
+
--- formarq.c
+++ formarq.c
@@ -3,22 +3,23 @@
 #include <string.h>
 #include <math.h>
 
-
 FILE *conversaoInvert(FILE *caminho, char diretorio[]);
 FILE *conversaoUToD(FILE *caminho, char diretorio[]);
-//FILE *conversaoDToU(FILE *caminho);
-//FILE *conversaoCript(FILE *caminho);
+FILE *conversaoCript(FILE *caminho, char diretorio[], char *chave);
 char somaUm(char caractere);
-char *retiraExtensao(char novoDiretorio[]); //Função que tira a extensão de um arquivo
+char *retiraExtensao(char novoDiretorio[]);
 char *pegaExtensao(char *diretorio);
+char ouExclusivo(char caractere, char chave);
 
 int main(int argc, char *argv[]) {
 
 	FILE *path;
-	char* comando;
+	char *comando;
+	char *chave;
 
 	path = fopen(argv[1], "r+b"); //Atribui o caminho do arquivo inserido à variavel path
 	comando = argv[2]; //Atribui o segundo argumento ao comando
+	chave = argv[3];
 
 	if(path == NULL) {
 		printf("Caminho inexistente!\n");
@@ -35,25 +36,22 @@ int main(int argc, char *argv[]) {
 				conversaoUToD(path, argv[1]);
 			break;
 
-			case 'D':
-				printf("Conversão D to U\n");
-				//conversaoDToU(path);
-			break;
-
 			default:
-				printf("Argumentos inválidos... [I = Invert; U = UNIX to DOS; D = DOS to UNIX]\n");
+				printf("Argumentos inválidos... [I = Invert; U = UNIX to DOS]\n");
 		}
 	} else {
 		if(argc == 4) { //Verifica o comando inserido caso quatro argumentos sejam inseridos
 			switch(*comando) {
 				case 'C':
 					printf("Conversão Cript\n");
-					//conversaoCript(path);
+					conversaoCript(path, argv[1], chave);
 				break;
 
 				default:
 					printf("Argumentos inválidos... [C = Cript]\n");
 			}
+		} else {
+			printf("Número de argumentos inválido [2 argumentos para conversão Invert, UNIX e 3 para Cript]\n");
 		}
 	}
 	}
@@ -73,29 +71,33 @@ FILE *conversaoUToD(FILE *caminho, char diretorio[]) {
 	extensao = pegaExtensao(diretorio);
 	x = retiraExtensao(novoDiretorio); //Tira a extensão do arquivo
 	strcpy(novoDiretorio, x);
-	printf("%s\n", extensao);
 
-	int resultado = strcmp(extensao, ".unx"); //Vê se o extensão é .unx
+	int resultado = strcmp(extensao, ".dos"); //Vê se o extensão é .unx
 	if(resultado == 0) {
-		strcat(novoDiretorio, ".dos");
+		strcat(novoDiretorio, ".unx");
+		novoArquivo = fopen(novoDiretorio, "w+");
+		while(ch != EOF) {
+			ch = fgetc(caminho);
+			if(ch == '\r') { //Se acha uma quebra de linha DOS
+				ch = 00;
+				fputc(ch, novoArquivo);
+			} else {
+				fputc(ch, novoArquivo);
+			}
+		}
 	} else {
-		printf("Formato de arquivo inválido! (Insira um arquivo .unx)\n");
-		exit(0);
-	}
-
-	novoArquivo = fopen(novoDiretorio, "w+");
-
-	while(ch != EOF) {
-		ch = fgetc(caminho);
-		printf("%c", ch);
-		if(ch == '\n') {
-			printf("\nAchei uma quebra de linha!\n" );
-			ch = '\r';
-			fputc(ch, novoArquivo);
-			ch = '\n';
-			fputc(ch, novoArquivo);
-		} else {
-			fputc(ch, novoArquivo);
+		strcat(novoDiretorio, ".dos");
+		novoArquivo = fopen(novoDiretorio, "w+");
+		while(ch != EOF) {
+			ch = fgetc(caminho);
+			if(ch == '\n') { //Se acha uma quebra de linha UNIX
+				ch = '\r';
+				fputc(ch, novoArquivo);
+				ch = '\n';
+				fputc(ch, novoArquivo);
+			} else {
+				fputc(ch, novoArquivo);
+			}
 		}
 	}
 
@@ -114,7 +116,6 @@ FILE *conversaoInvert(FILE *caminho, char diretorio[]) {
 	extensao = pegaExtensao(diretorio);
 	x = retiraExtensao(novoDiretorio); //Tira a extensão do arquivo
 	strcpy(novoDiretorio, x);
-	printf("%s\n", extensao);
 
 	int resultado = strcmp(extensao, ".inv"); //Vê se o extensão é .inv
 	if(resultado == 0) {
@@ -127,13 +128,46 @@ FILE *conversaoInvert(FILE *caminho, char diretorio[]) {
 
 	while(ch != EOF) {
 		ch = fgetc(caminho);
-		fputc(somaUm(ch), novoArquivo);
+		fputc(somaUm(ch), novoArquivo); //Faz complemento de um em cada caractere
 	}
 
 	if(x != NULL) {
 		printf("Novo diretório: %s\n", novoDiretorio);
 	} else {
 		strcat(novoDiretorio, ".inv");
+	}
+
+	return novoArquivo;
+}
+
+FILE *conversaoCript(FILE *caminho, char diretorio[], char *chave) {
+
+	char ch;
+	char novoDiretorio[strlen(diretorio)+2]; //String do novo arquivo que será criado
+	strcpy(novoDiretorio, diretorio);
+	FILE *novoArquivo; //Arquivo em si que será criado
+	char *x, *extensao;
+	int i;
+
+	extensao = pegaExtensao(diretorio);
+	x = retiraExtensao(novoDiretorio); //Tira a extensão do arquivo
+	strcpy(novoDiretorio, x);
+
+	int resultado = strcmp(extensao, ".crp"); //Vê se o extensão é .crp
+	if(resultado == 0) {
+		strcat(novoDiretorio, ".drp");
+	} else {
+		strcat(novoDiretorio, ".crp");
+	}
+
+		novoArquivo = fopen(novoDiretorio, "w+"); //Cria o arquivo
+
+	while(ch != EOF) {
+		for(i=0; i<strlen(chave); i++) {
+			ch = fgetc(caminho);
+			fputc(ouExclusivo(ch, chave[i]), novoArquivo);
+		}
+		i=0;
 	}
 
 	return novoArquivo;
@@ -148,20 +182,13 @@ char somaUm(char caractere) {
   char reverso;
 	float numReverso = 0;
 
-	printf("Caractere: %c\n", caractere);
-	printf("Número em decimal: %d\n", numero);
 	for(i=0;numero>0;i++) { //Gera o binário do número
 	arrayBinario[i]=numero%2;
 	numero=numero/2;
 	}
 	int tamanhoBinario = i;
-	printf("Número em binário: ");
-	for(i=6;i>=0; i--) {
-		printf("%d", arrayBinario[i]);
-	}
-	printf("\n");
 
-	for(i=0;i<7;i++) { //Soma um no binário
+	for(i=0;i<7;i++) { //Complemento de um
 		if(arrayBinario[i] == 0) {
 			arrayReverso[i] = 1;
 		} else {
@@ -169,16 +196,9 @@ char somaUm(char caractere) {
 		}
 	}
 
-	printf("Número binário reverso: ");
-	for(i=6;i>=0; i--) {
-		printf("%d", arrayReverso[i]);
-	}
-	printf("\n");
-
-	for(i=0;i<7;i++) {
+	for(i=0;i<7;i++) { //Gera decimal a partir do binário
 		numReverso += (arrayReverso[i] * pow(2, i));
 	}
-	printf("Número decimal reverso: %d\n", (int) numReverso);
 	reverso = (char) numReverso;
 
 	return reverso;
@@ -215,4 +235,42 @@ char *pegaExtensao(char *diretorio) {
 	}
 
 return extensao;
+}
+
+char ouExclusivo(char caractere, char chave) {
+
+	int i;
+	int binCar[7];
+	int binChave[7];
+	int binOu[7];
+	int numCar = (int) caractere;
+	int numChave = (int) chave;
+	char novoCaractere;
+	int numCh = 0;
+
+	for(i=0;i<7;i++) { //Gera o binário do número
+	binCar[i]=numCar%2;
+	numCar=numCar/2;
+	}
+
+	for(i=0;i<7;i++) { //Gera o binário do número
+	binChave[i]=numChave%2;
+	numChave=numChave/2;
+	}
+
+	for(i=0; i<7;i++) { //Ou exclusivo
+		if(binCar[i] == binChave[i]) {
+			binOu[i] = 1;
+		} else {
+			binOu[i] = 0;
+		}
+	}
+
+
+	for(i=0;i<7;i++) { //Gera decimal a partir do binário
+		numCh += (binOu[i] * pow(2, i));
+	}
+	novoCaractere = (char) numCh;
+
+	return novoCaractere;
 }
